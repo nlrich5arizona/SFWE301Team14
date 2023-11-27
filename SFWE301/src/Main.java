28751f42d00c378de6afebb8061e0d9b719294a4
Noah <noahrichey5@gmail.com>
2023-11-26 22:48:09 -0700

    Created the User class where admin permissions are assigned, made example in Main.java

--- SFWE301/src/Main.java
+++ SFWE301/src/Main.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        
+        User currUser = new User();
+        /* CODE NO LONGER WORKS
         Scholarship scholarship;
         scholarship = new Scholarship("Private University Scholarship","Chris Scholarship",10);
         System.out.println(scholarship);
+        */
+
+        String isAdminUserSelection;
+        System.out.println("Is the current user an administrator? (y/n)");
+        isAdminUserSelection = scnr.next();
+
+        if (isAdminUserSelection.equals("y")) {
+            currUser.GiveAdmin();
+        }
     }
 }
