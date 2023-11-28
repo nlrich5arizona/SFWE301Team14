48a49fe0c0ec1ba63496a9a24b3f4d0f10a009b5
dominickdemilio <132515000+dominickdemilio@users.noreply.github.com>
2023-11-28 00:23:53 -0700

    Add files via upload

---
+++ SFWE301/src/ApplicantLoader.java
@@ -0,0 +1,129 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ApplicantLoader {
+
+    public static Applicant loadApplicantData(String filePath) throws IOException, ParseException  {
+        // Applicant fields
+        String Name;
+        String Gender;
+        double GPA;
+        int Year_of_Study;
+        String Major;
+        String Minor;
+        boolean Transfer_Student;
+        int Student_ID;
+        String Email;
+        int units_enrolled;
+        String Citizenship;
+        Date Expected_Graduation_Date;
+        String Personal_Statement;
+        boolean Have_Funding;
+        String Club;
+        String Extracurriculars;
+        String Academic_Achievement;
+
+        // Set date format
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy");
+
+        // Read file
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            // To hold a line at a time
+            String line;
+
+            // While line is not empty
+            while ((line = reader.readLine()) != null) {
+                // Keep consistent format (": "), skip lines that don't match
+                String[] parts = line.split(": ");
+                if (parts.length < 2) continue;
+
+                // .trim() to remove whitespace should it exist in the scholarship info file
+                switch (parts[0].trim()) {
+                    case "Name":
+                        Name = parts[1].trim();
+                        break;
+
+                    case "Gender":
+                        Gender = parts[1].trim();
+                        break;
+
+                    case "GPA":
+                        GPA = Double.parseDouble(parts[1].trim());
+                        break;
+
+                    case "Year_of_Study":
+                        Year_of_Study = Integer.parseInt(parts[1].trim());
+                        break;
+
+                    case "Major":
+                        Major = parts[1].trim();
+                        break;
+
+                    case "Minor":
+                        Minor = parts[1].trim();
+                        break;
+
+                    case "Transfer_Student":
+                        Transfer_Student = parts[1].trim().equalsIgnoreCase("Yes"); // Assuming only options are Yes/No
+                        break;
+
+                    case "Student_ID":
+                        Student_ID = Integer.parseInt(parts[1].trim());
+                        break;
+
+                    case "Email":
+                        Email = parts[1].trim();
+                        break;
+
+                    case "Units_Enrolled_In":
+                        units_enrolled = Integer.parseInt(parts[1].trim());
+                        break;
+
+                    case "Citizenship":
+                        Citizenship = parts[1].trim().equalsIgnoreCase("Yes");
+                        break;
+
+                    case "Expected_Graduation_Date":
+                        Expected_Graduation_Date = dateFormat.parse(parts[1].trim());
+                        break;
+
+                    case "Already_have_funding":
+                        Have_Funding = parts[1].trim().equalsIgnoreCase("Yes"); // Assuming only options are Yes/No
+                        break;
+
+                    case "Personal_Statement":
+                        Personal_Statement = parts[1].trim();
+                        break;
+
+                    case "Clubs":
+                        Club = parts[1].trim();
+                        break;
+
+                    case "Extracurriculars":
+                        Extracurriculars = parts[1].trim();
+                        break;
+
+                    case "Academic_Achievements":
+                        Academic_Achievement = parts[1].trim();
+                        break;
+                }
+            }
+        } 
+        
+        // Handle NumberFormatException for parsing integer, double
+        catch (NumberFormatException e) {
+            throw new ParseException("Failed to parse numeric values in the file: " + filePath, 0);
+        }
+        
+        // Handle or log ParseException for parsing date
+        catch (ParseException e) {
+            throw new ParseException("Failed to parse date values in the file: " + filePath, 0);
+        }
+
+        return new StudentProfile(Name, Gender, GPA, Year_of_Study, Major, Minor, Transfer_Student, Student_ID, Email, units_enrolled, Citizenship, Expected_Graduation_Date, Personal_Statement, Have_Funding, Club, Extracurriculars, Academic_Achievement);
+    }
+}
