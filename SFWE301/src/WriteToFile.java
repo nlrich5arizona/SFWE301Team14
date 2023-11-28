86c96cddb3b85bd0d147e871be9ecc19742d4eeb
dominickdemilio <132515000+dominickdemilio@users.noreply.github.com>
2023-11-28 01:10:41 -0700

    Add files via upload

---
+++ SFWE301/src/WriteToFile.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ScholarshipAwardWriter {
+
+    public static void writeAwardToFile(String scholarshipName, String applicantName, int applicantId, String filePath) {
+
+        // Build output string
+        String content = String.format("The %s has been awarded to %s, ID: %d.", scholarshipName, applicantName, applicantId);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+            // Write to file
+            writer.write(content);
+            // New line
+            writer.newLine();
+        } 
+        
+        // Handle exception
+        catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+    }
+}
